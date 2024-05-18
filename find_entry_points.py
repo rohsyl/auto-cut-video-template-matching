382046f943263b8562c5d56509c7b59d94186aaf
rohsyl <syzin12@gmail.com>
2024-05-18 17:48:28 +0200

    optimize

--- find_entry_points.py
+++ find_entry_points.py
@@ -1,5 +1,7 @@
 import cv2
 import multiprocessing
+import sys
+import json
 
 template_path = './files/templates/'
 # video_path = './files/in/2024-05-13 22-36-24.mkv'
@@ -7,6 +9,7 @@ video_path = './files/in/2024-05-15_W_Blitz_2v2_SSvSS_Again.mp4'
 thread_count = 4
 similarity_ratio = 0.85
 kickback_ratio = 0.1
+skip_frames = 30
 
 templates_in = [
     'in_template_sov.png',
@@ -16,8 +19,6 @@ templates_out = [
     'out_template_sov.png',
     'out_template_allied.png'
 ]
-
-timecodes = []
 
 
 def main():
@@ -34,35 +35,51 @@ def main():
     split_total_frames = round(total_frames / thread_count)
     split_frames_group = split_frames_for_threads(total_frames, split_total_frames)
 
+    manager = multiprocessing.Manager()
+    timecodes = manager.dict()
+
     processes = []
     for i in range(thread_count):
-        t = multiprocessing.Process(target=process_segment, args=(i, split_frames_group[i], video_path, in_grays, out_grays))
-        t.start()
+        t = multiprocessing.Process(
+            target=process_segment,
+            args=(i, timecodes, split_frames_group[i], video_path, in_grays, out_grays)
+        )
         processes.append(t)
+        t.start()
 
     for t in processes:
         t.join()
 
     cv2.destroyAllWindows()
-    print(timecodes)
+    out = []
+    for proc in timecodes.values():
+        for frame in proc:
+            out.append(frame)
+    sys.stdout.write(json.dumps(out))
 
 
-def process_segment(thread_id, segment, path, in_templates, out_templates):
+def process_segment(thread_id, timecodes, segment, path, in_templates, out_templates):
+    tc = []
     cap = cv2.VideoCapture(path)
-    cap.set(cv2.CAP_PROP_POS_FRAMES, segment[0])
     fps = cap.get(cv2.CAP_PROP_FPS)
 
     kickback = [None, None, 1]
+
+    i = segment[0]
+    last_frame_ok = False
     while cap.isOpened():
+
+        if i > segment[1]:
+            i = segment[1] - 1
+            last_frame_ok = True
+            if last_frame_ok:
+                break
+
+        cap.set(cv2.CAP_PROP_POS_FRAMES, i)
+
         ret, frame = cap.read()
-
-        if frame is None:
-            break
-
         cframe = cap.get(cv2.CAP_PROP_POS_FRAMES)  # retrieves the current frame number
         # print('Reading : ' + str(cframe) + ' in thread id : ' + str(thread_id))
-        if cframe > segment[1]:
-            break
 
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -71,16 +88,20 @@ def process_segment(thread_id, segment, path, in_templates, out_templates):
             ['out', out_templates],
         ]
 
-        kickback = process_templates(gray_frame, templates, kickback, cframe, fps)
+        kickback = process_templates(tc, gray_frame, templates, kickback, cframe, fps)
+
+        i = i + skip_frames
 
     cap.release()
+    timecodes[i] = tc
 
 
-def process_templates(frame, g_templates, kickback, cframe, fps):
-    i = 0
-    j = 0
+def process_templates(timecodes, frame, g_templates, kickback, cframe, fps):
     if kickback[0] is None or kickback[1] is None:
+        find = False
+        i = 0
         for g_template in g_templates:
+            j = 0
             for template in g_template[1]:
                 max_val = get_similarity_ratio(frame, template)
                 if max_val > similarity_ratio:
@@ -92,17 +113,20 @@ def process_templates(frame, g_templates, kickback, cframe, fps):
                     kickback[0] = i
                     kickback[1] = j
                     kickback[2] = max_val - kickback_ratio
+                    find = True
+                    break
                 j += 1
+            if find:
+                break
             i += 1
     else:
+
         max_val = get_similarity_ratio(frame, g_templates[kickback[0]][1][kickback[1]])
         # skip next frame until we get under the kickback value again
         if max_val < kickback[2]:
             kickback[0] = None
             kickback[1] = None
             kickback[2] = 1
-        else:
-            print('skip')
 
     return kickback
 
